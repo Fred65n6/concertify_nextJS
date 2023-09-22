1583334cc7faf222c4723d628863c2dbc361948b
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-09-22 09:04:33 +0200

    placede conditional render til layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 // pages/index.js
 import React from "react";
-import Nav from "@/app/components/nav/page";
-import {hasCookie} from "@/helpers/cookieHelper";
 
 export default function Home() {
-    const showComponent = hasCookie("token");
-
     return (
         <div>
-            {showComponent ? (
-                // Render your component here
-                <Nav />
-            ) : // Render something else or nothing
-            null}
+            <h1>Home page</h1>
         </div>
     );
 }
