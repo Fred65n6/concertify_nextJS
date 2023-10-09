c5312be120573d225d97bfa7670533b31c287ea6
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-10-09 10:10:54 +0200

    search

--- src/app/components/search/page.jsx
+++ src/app/components/search/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import {useState} from "react";
-import SearchBar from "@/app/components/search/searchbar";
+import {SearchBar} from "@/app/components/search/searchbar";
 import {SearchResultsList} from "@/app/components/search/searchResultsList";
 
 function Search() {
