4151b9780b42ec60bd7458a893f14cc3e8cfe385
tomineodegard <tomi0105@stud.kea.dk>
2023-10-24 12:47:24 +0200

    genre is now visible on singleview

--- src/app/artists/[id]/page.tsx
+++ src/app/artists/[id]/page.tsx
@@ -9,6 +9,7 @@ import {SlStar, SlCalender, SlMusicToneAlt} from "react-icons/sl";
 
 import {HiOutlineArrowRight} from "react-icons/hi";
 import {PiBalloon} from "react-icons/pi";
+import ArtistCard from "@/app/components/artistCard/page";
 
 interface ArtistSingle {
     _id: string;
@@ -16,7 +17,10 @@ interface ArtistSingle {
     artist_description: string;
     artist_image: string;
     artist_dob: string;
-    // artist_genre: string;
+    artist_genre: {
+        genre_id: string;
+        genre_name: string;
+    };
 }
 
 export default function SingleArtist() {
@@ -51,91 +55,98 @@ export default function SingleArtist() {
 
     return (
         <div>
-            {selectedArtist ? (
-                <div className="grid grid-cols-1 md:grid-cols-2 md:gap-6">
-                    <figure>
-                        <Image
-                            src={"/" + selectedArtist.artist_image}
-                            width={200}
-                            height={200}
-                            alt="artist image"
-                            className="h-auto w-full rounded-lg"
-                        />
-                    </figure>
-                    <section>
-                        <h1 className="text-3xl font-bold my-2">
-                            {selectedArtist.artist_name}
-                        </h1>
+            <div>
+                {selectedArtist ? (
+                    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-6">
+                        <figure>
+                            <Image
+                                src={"/" + selectedArtist.artist_image}
+                                width={200}
+                                height={200}
+                                alt="artist image"
+                                className="h-auto w-full rounded-lg"
+                            />
+                        </figure>
+                        <section>
+                            <h1 className="text-3xl font-bold my-2">
+                                {selectedArtist.artist_name}
+                            </h1>
 
-                        <ul className="flex flex-col gap-4">
-                            {/* Full name*/}
-                            <li className="flex gap-2">
-                                <SlStar
-                                    className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
-                                    id="artist_fullname"
-                                />
-                                <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
-                                    Full name: {selectedArtist.artist_name}
-                                </p>
-                            </li>
-
-                            {/* Genre*/}
-                            <li className="flex gap-2">
-                                <SlMusicToneAlt
-                                    className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
-                                    id="artist_fullname"
-                                />
-                                {/* <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
-                                    Genre: {selectedArtist.artist_genre}
-                                </p> */}
-                            </li>
-
-                            {/* Date of birth*/}
-                            <li className="flex gap-2">
-                                <PiBalloon
-                                    className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
-                                    id="dob"
-                                />
-                                <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
-                                    Born:
-                                </p>
-                            </li>
-
-                            {/* Next concert near you*/}
-                            <li className="flex gap-2">
-                                <SlCalender
-                                    className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
-                                    id="see_all"
-                                />
-                                <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
-                                    Next concert near you:
-                                </p>
-                            </li>
-
-                            {/* See all concerts*/}
-                            <li className="flex gap-2">
-                                <Link className="flex gap-2" href="/concerts/">
-                                    <HiOutlineArrowRight
-                                        className="stroke-[#5311BF] dark:stroke-[#8e0bf5] w-5 h-5"
-                                        id="se_all"
+                            <ul className="flex flex-col gap-4">
+                                {/* Full name*/}
+                                <li className="flex gap-2">
+                                    <SlStar
+                                        className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
+                                        id="artist_fullname"
                                     />
-                                    <p className="text-[#5311BF] dark:text-[#8e0bf5] text-sm align-middle">
-                                        See all concerts
+                                    <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
+                                        Full name: {selectedArtist.artist_name}
                                     </p>
-                                </Link>
-                            </li>
-                        </ul>
+                                </li>
 
-                        <div className="border-t-[1px] border-[#979C9E] pt-4 mt-4">
-                            <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
-                                {selectedArtist.artist_description}
-                            </p>
-                        </div>
-                    </section>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+                                {/* Genre*/}
+                                <li className="flex gap-2">
+                                    <SlMusicToneAlt
+                                        className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
+                                        id="artist_genre"
+                                    />
+                                    <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
+                                        Genre: {selectedArtist?.artist_genre.genre_name}
+                                    </p>
+                                </li>
+
+                                {/* Date of birth*/}
+                                <li className="flex gap-2">
+                                    <PiBalloon
+                                        className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
+                                        id="dob"
+                                    />
+                                    <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
+                                        Born: {selectedArtist.artist_dob
+                                    ? selectedArtist?.artist_dob
+                                    : "Unknown"}
+                                    </p>
+                                </li>
+
+                                {/* Next concert near you*/}
+                                <li className="flex gap-2">
+                                    <SlCalender
+                                        className="fill-gray-600 text-sm dark:fill-gray-400 w-5 h-5"
+                                        id="see_all"
+                                    />
+                                    <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
+                                        Next concert near you: {selectedArtist.artist_next_concert
+                                    ? selectedArtist?.artist_next_concert
+                                    : "Unknown"}
+                                    </p>
+                                </li>
+
+                                {/* See all concerts*/}
+                                <li className="flex gap-2">
+                                    <Link className="flex gap-2" href="/concerts/">
+                                        <HiOutlineArrowRight
+                                            className="stroke-[#5311BF] dark:stroke-[#8e0bf5] w-5 h-5"
+                                            id="se_all"
+                                        />
+                                        <p className="text-[#5311BF] dark:text-[#8e0bf5] text-sm align-middle">
+                                            See all concerts
+                                        </p>
+                                    </Link>
+                                </li>
+                            </ul>
+
+                            {/* Artist description*/}
+                            <div className="border-t-[1px] border-[#979C9E] pt-4 mt-4">
+                                <p className="text-gray-600 text-sm dark:text-gray-400 align-middle">
+                                    {selectedArtist.artist_description}
+                                </p>
+                            </div>
+                        </section>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
     );
 }
