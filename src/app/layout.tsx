6c49a9a2ac847dbd4f203e2b3e506767b3b64acc
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-10-27 14:52:59 +0200

    aws key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,38 @@
 import "./globals.css";
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import type {Metadata} from "next";
+import {Plus_Jakarta_Sans} from "next/font/google";
 import Nav_logged from "@/app/components/navLoggedIn/page";
 import Nav_not_logged from "@/app/components/navNotLogged/page";
-import { hasCookie } from "@/helpers/cookieHelper";
+import {hasCookie} from "@/helpers/cookieHelper";
 import Footer from "./components/footer/page";
 
-const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const plusJakartaSans = Plus_Jakarta_Sans({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Concertify",
-  description: "Experience Copenhagen through live music",
+    title: "Concertify",
+    description: "Experience Copenhagen through live music",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const showComponent = hasCookie("token");
+export default function RootLayout({children}: {children: React.ReactNode}) {
+    const showComponent = hasCookie("token");
 
-  return (
-    <html lang="en">
-      <body>
-        <div
-          className={`${plusJakartaSans.className} dark:bg-[#202124] dark:text-white`}
-        >
-          {showComponent ? ( // Render your component here
-            <Nav_logged />
-          ) : (
-            <Nav_not_logged />
-          )}
-          <main className="max-w-[1300px] pt-8 m-auto px-8 lg:px-14 min-h-screen ">
-            {children}
-          </main>
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <div
+                    className={`${plusJakartaSans.className} dark:bg-[#202124] dark:text-white`}
+                >
+                    {showComponent ? ( // Render your component here
+                        <Nav_logged />
+                    ) : (
+                        <Nav_not_logged />
+                    )}
+                    <main className="max-w-[1300px] pt-8 m-auto px-8 lg:px-14 min-h-screen ">
+                        {children}
+                    </main>
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
