c7fccd7a8c95380f442a326b12e7c52818c55246
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-11-06 13:27:15 +0100

    _aws

--- src/app/api/data/uploadArtist/route.ts
+++ src/app/api/data/uploadArtist/route.ts
@@ -6,9 +6,9 @@ import AWS from "aws-sdk";
 
 // Load AWS credentials and configuration from environment variables
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION,
 });
 
 const s3 = new AWS.S3();
