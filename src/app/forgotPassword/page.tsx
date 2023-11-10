e11521ba59e45ec0698224841e8c37acce6b03cb
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-11-10 14:25:16 +0100

    hello

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-export default function SingupPage() {
+export default async function SingupPage() {
     const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
