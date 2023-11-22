2e33af2d1378cbe0b40087c9448808210844323f
fred65n6 <fred65n6@stud.kea.dk>
2023-11-22 15:08:24 +0100

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
   const isAdminPath = path === '/admin-dashboard' || path === '/admin-artist' || path === '/admin-concerts' || path === '/admin-upload-artist' || path === '/admin-uploadconcert' || path === '/admin-upload-venue' || path === '/admin-venues';
-  const isRestrictedPath = path === '/favourites' || path === '/profile';
+  const isRestrictedPath = path === '/favourites' || path === '/profile' || path === '/profile/${data}';
 
   const token = request.cookies.get('token')?.value || ''
   const adminToken = request.cookies.get('adminToken')?.value || ''
@@ -15,7 +15,7 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
 
-  if (isRestrictedPath && (!token || !adminToken)) {
+  if (isRestrictedPath && !token && !adminToken) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
     
@@ -36,6 +36,7 @@ export const config = {
     '/admin-upload-artist',
     '/admin-upload-venue',
     '/admin-upload-concert',
+    '/profile/${data}'
   ]
 }
 
