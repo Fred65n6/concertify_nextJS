cf47273c92c9ad1faa628d96de9d38a99f5ffc7d
tomineodegard <tomi0105@stud.kea.dk>
2023-11-22 16:34:39 +0100

    stying of log in and sign up modals

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,6 +7,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { Progress } from "flowbite-react";
 import { SiFacebook, SiGoogle, SiApple } from "react-icons/si";
+import { CgClose } from "react-icons/cg";
+
 
 export default function SignupPage() {
   const router = useRouter();
@@ -114,102 +116,104 @@ export default function SignupPage() {
     }
   }, [user]);
 
-  //TEMPLATE FOR SIGNUP
 
   return (
-    <div className="">
-      <div
-        id="signup_module"
-        className="absolute top-0 left-0 bg-slate-900/50 w-full h-screen  items-center justify-center hidden backdrop-blur-sm z-50"
-      >
+    <>
+      <div id="signup_module" className="absolute top-0 left-0 bg-slate-900/50 w-full h-screen items-center justify-center hidden backdrop-blur-sm z-50"      >
         <div id="signup_form">
-          <div className="flex flex-col items-center justify-center pt-4 py-8 bg-white w-[400px] md:w-[600px] lg:w-[800px] dark:bg-[#202124] dark:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:shadow-white/20 rounded-lg">
+          <div className="p-10 mx-4 md:m-0 flex flex-col items-center w-fill md:w-[800px] bg-white rounded-lg dark:bg-[#202124]">
             <button
-              type="button"
-              onClick={closeSignupModule}
-              className="cursor-pointer ml-[75%]"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m7 7l10 10M7 17L17 7"
-                />
-              </svg>
-            </button>
-            <h1 className="mb-4 text-3xl font-bold">
-              {loading ? "Processing" : "Signup..."}
-            </h1>
-            <p className="mb-6">To get personalized recommendations</p>
-            <hr />
-            {error && <div className="text-red-500">{error}</div>}
-            <label htmlFor="username">Username</label>
-            <input
-              className="m-2 mt-1 p-2 pl-4 rounded-full text-left text-black bg-slate-200 w-[350px] lg:w-[400px]"
-              type="text"
-              id="username"
-              value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
-              placeholder="Username"
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              className="m-2 mt-1 p-2 pl-4 rounded-full text-left text-black bg-slate-200 w-[350px] lg:w-[400px]"
-              type="text"
-              id="email"
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              placeholder="Email"
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              className="m-2 mt-1 p-2 pl-4 rounded-full text-left text-black bg-slate-200 w-[350px] lg:w-[400px]"
-              type="password"
-              id="password"
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              placeholder="Password"
-            />
-            <label htmlFor="confirmpassword">Confirm password</label>
-            <input
-              className="m-2 mt-1 p-2 pl-4 rounded-full text-left text-black bg-slate-200 w-[350px] lg:w-[400px]"
-              type="password"
-              id="confirmpassword"
-              value={user.confirmpassword}
-              onChange={(e) =>
-                setUser({ ...user, confirmpassword: e.target.value })
-              }
-              placeholder="Confirm your password"
-            />
+                  type="button"
+                  onClick={closeSignupModule}
+                  className="cursor-pointer ml-[100%]"
+                  >
+                  <CgClose/>
+              </button>
+
+              <span className="mb-4 text-3xl font-bold dark:text-black">
+                {loading ? "Processing" : "Sign up"}
+              </span>
+              <p className="mb-6">To get a personalised experience</p>
+              <div className="flex flex-col gap-4 w-full">
+                <div className="flex flex-col gap-4 w-full">
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="username" className="w-fit text-sm text-gray-600">Username</label>
+                    <input
+                      className="bg-slate-100 border-0 px-8 py-4 rounded-full w-full"
+                      type="text"
+                      id="username"
+                      value={user.username}
+                      onChange={(e) => setUser({ ...user, username: e.target.value })}
+                      placeholder="Username"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col w-full gap-2">
+                  <label htmlFor="email" className="w-fit text-sm text-gray-600">Email</label>
+                  <input
+                    className="bg-slate-100 border-0 px-8 py-4 rounded-full w-full"
+                    type="text"
+                    id="email"
+                    value={user.email}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    placeholder="Email"
+                  />
+                </div>
+
+                <div className="flex flex-col w-full gap-2">
+                  <label htmlFor="password" className="w-fit text-sm text-gray-600">Password</label>
+                  <input
+                  className="bg-slate-100 border-0 px-8 py-4 rounded-full w-full"
+                    type="password"
+                    id="password"
+                    value={user.password}
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    placeholder="Password"
+                  />
+                </div>
+
+                <div className="flex flex-col w-full gap-2">
+                  <label htmlFor="confirmpassword" className="w-fit text-sm text-gray-600">Confirm password</label>
+                  <input
+                    className="bg-slate-100 border-0 px-8 py-4 rounded-full w-full"
+                    type="password"
+                    id="confirmpassword"
+                    value={user.confirmpassword}
+                    onChange={(e) =>
+                      setUser({ ...user, confirmpassword: e.target.value })
+                    }
+                    placeholder="Confirm your password"
+                  />
+                </div>
+              </div>
+
+              {error && <div className="text-red-500">{error}</div>}
+            
             <button
               onClick={onSignup}
-              className="mb-4 mt-2 brand_gradient px-12 py-4 rounded-full text-white"
+              className="mb-4 mt-8 brand_gradient px-12 py-4 rounded-full text-white"
             >
-              {buttonDisabled ? "Missing fields" : "Sign up"}
+              {buttonDisabled ? "Please fill out all fields to sign up..." : "Sign up"}
             </button>
-            <div className="flex">
+
+            <div className="flex gap-4">
+              <span></span>
               <Link href="/">
                 <SiGoogle
-                  className="stroke-gray-600 dark:stroke-[#5311BF] w-4 h-4"
+                  className="fill-gray-600 dark:fill-[#5311BF] w-6 h-6"
                   id="google"
                 />
               </Link>
               <Link href="/">
                 <SiFacebook
-                  className="stroke-gray-600 dark:stroke-[#5311BF] w-4 h-4"
+                  className="fill-gray-600 dark:fill-[#5311BF] w-6 h-6"
                   id="facebook"
                 />
               </Link>
               <Link href="/">
                 <SiApple
-                  className="stroke-gray-600 dark:stroke-[#5311BF] w-4 h-4"
+                  className="fill-gray-600 dark:fill-[#5311BF] w-6 h-6"
                   id="apple"
                 />
               </Link>
@@ -228,6 +232,7 @@ export default function SignupPage() {
             </div>
           </div>
         </div>
+
         {/* WELCOME POPUP */}
         <div id="welcome_popup" className="hidden">
           <div className="flex flex-col items-center justify-center py-8 bg-white w-[400px] md:w-[600px] lg:w-[800px] dark:bg-[#202124] dark:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:shadow-white/20 rounded-lg">
@@ -253,6 +258,7 @@ export default function SignupPage() {
             </button>
           </div>
         </div>
+
         {/* PROFILE GENRES MODUL */}
         <div id="profile_genres" className="hidden">
           <div className="flex flex-col items-center justify-center py-8 bg-white w-[400px] md:w-[600px] lg:w-[800px] dark:bg-[#202124] dark:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:shadow-white/20 rounded-lg">
@@ -279,6 +285,7 @@ export default function SignupPage() {
             </button>
           </div>
         </div>
+
         {/* PROFILE VENUES MODUL */}
         <div id="profile_venues" className="hidden">
           <div className="flex flex-col items-center justify-center py-8 bg-white w-[400px] md:w-[600px] lg:w-[800px] dark:bg-[#202124] dark:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:shadow-white/20 rounded-lg">
@@ -319,6 +326,6 @@ export default function SignupPage() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
