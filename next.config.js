1267622254eacc66be68b76ee0aaeb8735167d91
fred65n6 <fred65n6@stud.kea.dk>
2024-01-10 15:54:08 +0100

    config

--- next.config.js
+++ next.config.js
@@ -3,34 +3,33 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,  
     async headers() {
-      return [
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'X-Frame-Options',
-                value: 'DENY',
-              },
-              {
-                key: 'Content-Security-Policy',
-                value:
-                "default-src 'self' https://concertify.netlify.app; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline'",
-                },
-              {
-                key: 'X-Content-Type-Options',
-                value: 'nosniff',
-              },
-              {
-                key: 'Permissions-Policy',
-                value: "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
-              },
-              {
-                key: 'Referrer-Policy',
-                value: 'origin-when-cross-origin',
-              },
-            ],
-          },
-      ];
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self' https://concertify.netlify.app; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline'",
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                ],
+            },
+        ];
     },   
 };
 
