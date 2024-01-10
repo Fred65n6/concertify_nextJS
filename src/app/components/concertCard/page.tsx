426fe097b037ad93a8c26e0343fd1295637a83da
fred65n6 <fred65n6@stud.kea.dk>
2024-01-10 16:02:03 +0100

    netilify.toml

--- src/app/components/concertCard/page.tsx
+++ src/app/components/concertCard/page.tsx
@@ -3,8 +3,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
-
 interface ConcertCard {
   concert_id: string;
   concert_artist: {
