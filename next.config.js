88ffb405c50d12e94770e4124fb116ea2d3182b8
Emilie Schultz <emil07v9@stud.kea.dk>
2024-01-10 20:39:38 +0100

    changing source in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,53 +1,55 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,  
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'self' https://concertify.netlify.app; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline'",
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'Permissions-Policy',
-                        value: "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
-                    },
-                    {
-                        key: 'Referrer-Policy',
-                        value: 'origin-when-cross-origin',
-                    },
-                ],
-            },
-        ];
-    },   
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/.",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' https://concertify.netlify.app; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = {
-    async redirects() {
-        return [
-          {
-            source: '/_headers',
-            destination: '/_headers',
-            permanent: true,
-          },
-        ];
+  async redirects() {
+    return [
+      {
+        source: "/_headers",
+        destination: "/_headers",
+        permanent: true,
       },
-    ...nextConfig,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: {
-        domains: ["storage.googleapis.com"],
-    },
+    ];
+  },
+  ...nextConfig,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
 };
