e8eb8ab6f05622618d006d8d57fb3af77ead7202
fred65n6 <fred65n6@stud.kea.dk>
2024-01-11 08:38:22 +0100

    fjernet serverside props

--- src/app/api/data/venueData/route.ts
+++ src/app/api/data/venueData/route.ts
@@ -4,19 +4,6 @@ import { connect } from "@/dbConfig/dbConfig";
 
 connect();
 
-export async function getServerSideProps() {
-  return {
-    props: {},
-    headers: {
-      // Add your headers for API routes here
-      "X-Frame-Options": "DENY",
-      "Content-Security-Policy":
-        "default-src 'self' https://concertify.netlify.app;",
-      // Add other headers as needed
-    },
-  };
-}
-
 export async function GET(request: NextRequest, response: NextResponse) {
   try {
     const venue = await Venue.find();
